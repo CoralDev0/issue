cf14b1096f4f2a7fea1ce16535e8de1dc941ee1f
yin & yang <116958279+yinyangbalance@users.noreply.github.com>
2024-06-24 21:54:44 +0800

    Create external.bat

---
+++ external.bat
@@ -0,0 +1,5 @@
+@echo off
+echo external is in currently in beta
+echo external is still in development
+echo external is a exe but dosent need dependencies. Cause everything is compiled in one file exe
+pause
